74090cdfb9f12c317d6c98105da6bd31fdf1779c
Jason Chen <jason.chen@uwaterloo.ca>
2016-08-30 11:30:09 -0400

    tried to add checkboxes to FX area

--- public/admin_2/assets/jquery-datatables-editable/datatables.editable.init.js
+++ public/admin_2/assets/jquery-datatables-editable/datatables.editable.init.js
@@ -1319,4 +1319,15 @@ var DirectTerritorySalesTable = {
 	NonRecoupingTable.initialize();
 
 
+
 }).apply(this, [ jQuery ]);
+
+$("#Submit").on('click', function(e){
+		.alert('it works')
+		e.preventDefault
+		if($("#car").is(":checked")) {
+			$(".panel-body").append("<b>Appended text</b>");
+		} else {
+			$(".panel-body").append("<b>Appended text</b>");
+		}
+});
