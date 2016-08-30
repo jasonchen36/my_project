2dc32fc81782163eb284bcbaf5af425a3c511516
Jason Chen <jason.chen@uwaterloo.ca>
2016-08-30 11:59:42 -0400

    Moved the submit button jquery to a different file

--- public/admin_2/assets/jquery-datatables-editable/datatables.editable.init.js
+++ public/admin_2/assets/jquery-datatables-editable/datatables.editable.init.js
@@ -1320,13 +1320,3 @@ var DirectTerritorySalesTable = {
 
 
 }).apply(this, [ jQuery ]);
-
-$("#Submit").on('click', function(e){
-		.alert('it works')
-		e.preventDefault
-		if($("#car").is(":checked")) {
-			$(".panel-body").append("<b>Appended text</b>");
-		} else {
-			$(".panel-body").append("<b>Appended text</b>");
-		}
-});
---
+++ public/admin_2/js/FX.js
@@ -0,0 +1,11 @@
+$(function(){
+  $("#Submit").on('click', function(e){
+    .alert('it works')
+    e.preventDefault
+    if($("#car").is(":checked")) {
+      $(".panel-body").append("<b>Appended text</b>");
+    } else {
+      $(".panel-body").append("<b>Appended text</b>");
+    }
+  });
+});
