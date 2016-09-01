cda67526d8adee8194efdf8615b31b32bbe8cf0f
Aaron Kelly-Barker <aaron.kelly.barker@gmail.com>
2016-09-01 15:19:10 -0400

    Fixed direct territory sales and added one data row

--- public/admin_2/js/production.js
+++ public/admin_2/js/production.js
@@ -98,43 +98,39 @@ var summaryTableRow = Handlebars.templates["summaryTableRow"];
 
 (function() {
 	var idCount = 1;
-
 	//Create modals for presales
 	var directTerritoryModalTemplate = Handlebars.templates['directTerritoryModal'];
 	var directTerritoryAdd = {id: "add", investor: "", amount: "0", currency: "CAD", territory: "", equity: "0", overages: "0", hasPremium: "", premium: "0", hasArrangement: "", isFront:"", arrangement: ""};
-	var presalesData1 = {id: "1", investor: "Concourse Media", amount: "100000", currency: "CAD", territory: "United Kingdom", depositAdvances: "0", escrowAmount: "0", equity: "20"};
+	var directTerritoryData1 = {id: "1", investor: "French Distributors", amount: "275000", currency: "CAD", territory: "France", equity: "4", overages: "60", hasPremium: "", premium: "0", hasArrangement: "", isFront:"", arrangement: ""};
 
 	$("#modalLocation")
 		.append(directTerritoryModalTemplate(directTerritoryAdd))
-		// .append(presaleModalTemplate(presalesData1));
+		.append(directTerritoryModalTemplate(directTerritoryData1));
 
 	var addEditListener = function() {
 		var id = idCount;
-		$("#pre-sales-"+id+"-edit").on('click', function() {
-			var investor = $("#pre-sales-"+id+"-investor").val();
-			var territory = $("#pre-sales-"+id+"-territory").val();
-			var currency = $("#pre-sales-"+id+"-currency").val();
-			var amount = $("#pre-sales-"+id+"-amount").val();
-			var depositAdvances = $("#pre-sales-"+id+"-depositAdvances").val();
-			var escrowAmount = $("#pre-sales-"+id+"-escrow-amount").val();
-			var data = {id: id, investor: investor, territory: territory, currency: currency, amount: amount, depositAdvances: depositAdvances, escrowAmount: escrowAmount, type: "pre-sales" };
-			// $("#pre-sales-"+id+"-modal").replaceWith(presaleModalTemplate(data));
+		$("#direct-territory-"+id+"-edit").on('click', function() {
+			var investor = $("#direct-territory-add-investor").val();
+			var territory = $("#direct-territory-add-territory").val();
+			var currency = $("#direct-territory-add-currency").val();
+			var amount = $("#direct-territory-add-amount").val();
+			var data = {id: idCount, investor: investor, territory: territory, currency: currency, amount: amount, type: "direct-territory" };			// $("#pre-sales-"+id+"-modal").replaceWith(presaleModalTemplate(data));
 			// $("#pre-sales-table-row-"+id).innerHTML = "HIIII";
-			$("#pre-sales-"+id+"-form").trigger('reset');
+			$("#direct-territory-"+id+"-form").trigger('reset');
 		});
 
-		$("#pre-sales-"+id+"-cancel").on('click', function() {
-			$("#pre-sales-"+id+"-form").trigger('reset');
+		$("#direct-territory-"+id+"-cancel").on('click', function() {
+			$("#direct-territory-"+id+"-form").trigger('reset');
 		});
 
-		$("#pre-sales-"+id+"-close").on('click', function() {
-			$("#pre-sales-"+id+"-form").trigger('reset');
+		$("#direct-territory-"+id+"-close").on('click', function() {
+			$("#direct-territory-"+id+"-form").trigger('reset');
 		});
 
 	};
 
-	// addEditListener();
-	// idCount+=1;
+	addEditListener();
+	idCount+=1;
 	$("#direct-territory-add-save").on('click', function() {
 		var investor = $("#direct-territory-add-investor").val();
 		var territory = $("#direct-territory-add-territory").val();
@@ -143,8 +139,8 @@ var summaryTableRow = Handlebars.templates["summaryTableRow"];
 		var data = {id: idCount, investor: investor, territory: territory, currency: currency, amount: amount, type: "direct-territory" };
 		$("#modalLocation").append(directTerritoryModalTemplate(data));
 		$("#direct-territory-summary-table").append(summaryTableRow(data));
-		// addEditListener();
-		// idCount+= 1;
+		addEditListener();
+		idCount+= 1;
 		$("#direct-territory-add-form").trigger('reset');
 	});
 
